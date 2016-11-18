b5c837c729fe9a305abb3eaff3d8b2bda06f0ca6
Madison Flannery <madisonflannery93@gmail.com>
2016-11-18 16:12:14 +1100

    Remove now redundant files

--- roles/web_apollo/files/setenv.sh
+++
@@ -1,5 +0,0 @@
-export CATALINA_OPTS="-Xms512m -Xmx1g \
-        -XX:+CMSClassUnloadingEnabled \
-        -XX:+CMSPermGenSweepingEnabled \
-        -XX:+UseConcMarkSweepGC \
-        -XX:MaxPermSize=256m"
